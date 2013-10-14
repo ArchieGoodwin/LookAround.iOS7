4f38762c710f32101a1920cf3f6234a45b0b16ce
ArchieGoodwin <nerowolfe@me.com>
2013-10-14 15:51:54 +0400

    Initial Commit

---
+++ LookAround.iOS7/DKAViewController.h
@@ -0,0 +1,13 @@
+//
+//  DKAViewController.h
+//  LookAround.iOS7
+//
+//  Created by Nero Wolfe on 14/10/13.
+//  Copyright (c) 2013 Sergey Dikarev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DKAViewController : UIViewController
+
+@end
